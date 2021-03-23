88c3414b24ab9ef5246d0c5bb108c0e1a6148988
Claire VM <claireg727@gmail.com>
2021-03-23 10:50:18 -0400

    added code for iteration

--- hash.py
+++ hash.py
@@ -1,1 +1,10 @@
 # This code is meant to go through all the files except specified ones
+
+import os 
+
+rootDir = '/'
+for root, subdirectories, files in os.walk(rootDir):
+	for subdirectory in subdirectories:
+		print(os.path.join(root, subdirectory))
+	for file in files:
+		print(os.path.join(root, file))
