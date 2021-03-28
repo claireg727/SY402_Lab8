ab974d718085b2bcda4bd1ed5c2e172c9de905f7
Claire VM <claireg727@gmail.com>
2021-03-28 17:56:20 -0400

    added comments to code

--- hash.py
+++ hash.py
@@ -1,10 +1,11 @@
 # This code is meant to go through all the files except specified ones
 
-import os
-import hashlib 
-import csv
-import datetime
+import os               # used for the os.walk() function
+import hashlib          # used to hash the files
+import csv              # used to create and write to a CSV file 
+import datetime         # used to obtain the current time and date
 
+# This is the list of directories not to traverse through
 unhashableLst = [
 	'/dev',
 	'/proc',
@@ -15,6 +16,7 @@ unhashableLst = [
 	'/var/run'
 ]
 
+# This list contains each row of the output CSV file where we will store the information
 csvRows = []
 
 # os.walk to iterate 
@@ -24,10 +26,11 @@ for root, subdirectories, files in os.walk(rootDir):
 	for file in files:
 		#Source Link: geeksforgeeks.org/python-check-if-string-starts-with-any-element-in-list/
 		unhashable = root.startswith(tuple(unhashableLst))
-		if unhashable == True:
+		if unhashable == True:                            # if unhashable directory specified, move to next one
 			continue
-		else:
+		else:                                             # if not, save the file name
 			fileName = os.path.join(root, file)
+			# open file, hash it, and store the file name, the hash, and the time and date
 			try:
 				f = open(fileName, 'rb')
 				bytesToRead = f.read()
@@ -38,9 +41,10 @@ for root, subdirectories, files in os.walk(rootDir):
 				row = [fileName, hash, timeStamp]
 				csvRows.append(row)
 				f.close()
-			except IOError:
+			except IOError:  # if file can't open (not all files listed in the system actually exist), move on to next file
 				continue
 
+# open the output CSV file and add the contents of csvRows
 with open('filehashes.csv', 'w') as csvfile:
 	csvwriter = csv.writer(csvfile)
 	fields = ['File Path', 'Hash', 'Date and Time']
